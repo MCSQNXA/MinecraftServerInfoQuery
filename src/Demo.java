002568f31ea4908177d20f63de1277630a2d4846
MCSQNXA <3337140142@qq.com>
2021-12-05 16:32:09 +0800

    2021-12-05-16-32

--- src/Demo.java
+++ src/Demo.java
@@ -7,20 +7,10 @@ public class Demo {
 
 
     public static void main(String[] args) {
-        ArrayList<String> list = new MinecraftServerInfoQuery().query("mc.mcsq.cc", 19132);
+        System.out.println(MinecraftServerInfoQuery.queryJava("mc.mcsq.cc", 25565));
+        //System.out.println(MinecraftServerInfoQuery.queryBedrock("mc.mcsq.cc", 19132));
 
-        if (list.size() > 0) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("[服务器名称]:").append(list.get(1)).append("\n");
-            builder.append("[服务器版本]:").append(list.get(3)).append("\n");
-            builder.append("[服务器在线]:").append(list.get(4)).append("/").append(list.get(5)).append("\n");
-            builder.append("[服务器延迟]:").append(list.get(0)).append("\n");
-            builder.append("[服务器模式]:").append(list.get(8));
 
-            System.out.println(builder);
-        } else {
-            System.out.println("网络错误");
-        }
     }
 
 
--- src/com/mcsqnxa/common/MinecraftServerInfoQuery.java
+++ src/com/mcsqnxa/common/MinecraftServerInfoQuery.java
@@ -1,14 +1,78 @@
 package com.mcsqnxa.common;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MinecraftServerInfoQuery {
-    public static ArrayList<String> query(String address, int port) {
-        ArrayList<String> list = new ArrayList<>();
+
+    /**
+     * @param address 爪哇版服务器地址
+     * @param port    爪哇版服务器端口
+     * @return 查询成功，返回服务器信息
+     */
+    public static HashMap<String, String> queryJava(String address, int port) {
+        HashMap<String, String> map = new HashMap<>();
+
+        try {
+            Socket socket = new Socket(address, port);
+            socket.setSoTimeout(8000);
+
+            OutputStream os = socket.getOutputStream();
+            os.write(new byte[]{0x10, 0x00, 0x6B, 0x0A});
+            os.write(address.getBytes());
+            os.write(new byte[]{(byte) 0xDD, 0x63, 0x01, 0x01, 0x00});
+            os.flush();
+            socket.shutdownOutput();
+
+            InputStream is = socket.getInputStream();
+            is.read();
+            is.read();
+            is.read();//去除头部无用字节数据
+
+            int len;
+            ByteArrayOutputStream buff = new ByteArrayOutputStream(128);
+
+            while ((len = is.read()) != -1) {
+                buff.write(len);
+            }
+
+            JSONObject data = new JSONObject(buff.toString());
+            JSONObject description = data.getJSONObject("description");
+            JSONObject players = data.getJSONObject("players");
+            JSONObject version = data.getJSONObject("version");
+
+            map.put("服务器名称", description.getString("text"));
+            map.put("服务器在线", new StringBuilder().append(players.getInt("online")).append("/").append(players.getLong("max")).toString());
+            map.put("服务器版本", version.getString("name"));
+            map.put("服务器协议", String.valueOf(version.getInt("protocol")));
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return map;
+    }
+
+
+    /**
+     * @param address 基岩版服务器地址
+     * @param port    基岩版服务器端口
+     * @return 查询成功，返回服务器信息
+     */
+    public static HashMap<String, String> queryBedrock(String address, int port) {
+        HashMap<String, String> map = new HashMap<>();
 
         try {
             byte[] data = new byte[]{
@@ -17,8 +81,6 @@ public class MinecraftServerInfoQuery {
                     (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0xAF, (byte) 0x36, (byte) 0x9C, (byte) 0xAC, (byte) 0x81, (byte) 0x36, (byte) 0x1D, (byte) 0x03
             };
 
-            //耗时start
-            long start = System.currentTimeMillis();
             //创建UDP
             DatagramSocket socket = new DatagramSocket();
             socket.connect(new InetSocketAddress(address, port));
@@ -29,30 +91,52 @@ public class MinecraftServerInfoQuery {
             DatagramPacket packet = new DatagramPacket(new byte[102400], 102400);
             //接收返回数据;阻塞
             socket.receive(packet);
-            //耗时
-            list.add(System.currentTimeMillis() - start + "Ms");
             //获取有效字节长度,并且创建缓存区
             byte[] receive = new byte[packet.getLength() - 40];
             //取出有效数据
             System.arraycopy(packet.getData(), 40, receive, 0, packet.getLength() - 40);
 
-            int k = 0;
-            for (int i = 0; i < receive.length; i++) {
+            for (int i = 0, k = 0, c = 0; i < receive.length; i++) {
                 if (receive[i] == ';') {
                     byte[] buffer = new byte[i - k];
 
-                    for (int p = 0; p < buffer.length; p++) {
+                    for (int p = 0; p < buffer.length; p++, k++) {
                         buffer[p] = receive[k];
-                        k++;
                     }
 
                     k++;
-                    list.add(new String(buffer));
-                }
-            }
+                    c++;
 
-            if (list.size() != 12) {
-                list.clear();
+                    switch (c) {
+                        case 1:
+                            map.put("服务器名称", new String(buffer));
+                            break;
+
+                        case 2:
+                            map.put("服务器协议", new String(buffer));
+                            break;
+
+                        case 3:
+                            map.put("服务器版本", new String(buffer));
+                            break;
+
+                        case 4:
+                            map.put("服务器在线", new String(buffer));
+                            break;
+
+                        case 5:
+                            map.put("服务器在线", new StringBuilder().append(map.get("服务器在线")).append("/").append(new String(buffer)).toString());
+                            break;
+
+                        case 7:
+                            map.put("服务器存档", new String(buffer));
+                            break;
+
+                        case 8:
+                            map.put("服务器模式", new String(buffer));
+                            break;
+                    }
+                }
             }
 
             socket.close();
@@ -60,7 +144,7 @@ public class MinecraftServerInfoQuery {
             e.printStackTrace();
         }
 
-        return list;
+        return map;
     }
 
 
